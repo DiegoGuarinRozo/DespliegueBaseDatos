5a0463a00d941fd083dd7ebd30c468c32bcf095f
DiegoGuarinRozo <diego19guro@gmail.com>
2022-11-04 19:19:41 -0500

    Se agregan nuevos cambios

--- DespliegueDB/HumanResources/Tables/Department.sql
+++ DespliegueDB/HumanResources/Tables/Department.sql
@@ -1,15 +1,19 @@
 ﻿CREATE TABLE [HumanResources].[Department] (
     [DepartmentID] SMALLINT     IDENTITY (1, 1) NOT NULL,
-    [Name]         [dbo].[Name] NOT NULL,
+    [Names]        [dbo].[Name] NOT NULL,
     [GroupName]    [dbo].[Name] NOT NULL,
     [ModifiedDate] DATETIME     CONSTRAINT [DF_Department_ModifiedDate] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_Department_DepartmentID] PRIMARY KEY CLUSTERED ([DepartmentID] ASC)
 );
 
 
+
+
 GO
 CREATE UNIQUE NONCLUSTERED INDEX [AK_Department_Name]
-    ON [HumanResources].[Department]([Name] ASC);
+    ON [HumanResources].[Department]([Names] ASC);
+
+
 
 
 GO
@@ -25,7 +29,7 @@ EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Primary key
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Name of the department.', @level0type = N'SCHEMA', @level0name = N'HumanResources', @level1type = N'TABLE', @level1name = N'Department', @level2type = N'COLUMN', @level2name = N'Name';
+
 
 
 GO
@@ -42,4 +46,8 @@ EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Default con
 
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Primary key (clustered) constraint', @level0type = N'SCHEMA', @level0name = N'HumanResources', @level1type = N'TABLE', @level1name = N'Department', @level2type = N'CONSTRAINT', @level2name = N'PK_Department_DepartmentID';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Name of the department.', @level0type = N'SCHEMA', @level0name = N'HumanResources', @level1type = N'TABLE', @level1name = N'Department', @level2type = N'COLUMN', @level2name = N'Names';
 
